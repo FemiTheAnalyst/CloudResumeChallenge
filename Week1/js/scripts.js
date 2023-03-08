d2835a9534a4b71790ccd6ee3e501cb867d9704f
Olufemi Ijaola <58881494+FemiTheAnalyst@users.noreply.github.com>
2023-03-08 12:04:31 +0100

    Update scripts.js

--- Week1/js/scripts.js
+++ Week1/js/scripts.js
@@ -1,1 +1,18 @@
 
+"use strict";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://hw3w8zp4xc.execute-api.us-east-1.amazonaws.com/Prod/visit", {
+    method: "POST"
+    })
+    .then(response => response.text())
+    .then(visitorCounter => {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("#visits").textContent = visitorCounter;
+    })
+    .catch(error => {
+    console.log("Error");
+    console.log(error);
+    });
+});

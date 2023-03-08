516ea2139c7e7bbbfae40517ae9741df5b2d80ed
Olufemi Ijaola <58881494+FemiTheAnalyst@users.noreply.github.com>
2023-03-08 15:41:48 +0100

    Update scripts.js

--- Week 2/resume/js/scripts.js
+++ Week 2/resume/js/scripts.js
@@ -1,1 +1,18 @@
+"use strict";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("YOUR_AWS_API_ENDPOINT_HERE", {
+    method: "POST"
+    })
+    .then(response => response.text())
+    .then(visitorCounter => {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("#visits").textContent = visitorCounter;
+    })
+    .catch(error => {
+    console.log("Error");
+    console.log(error);
+    });
+});
 
